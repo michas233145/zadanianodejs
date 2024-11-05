8ddf251d47fdd334fd8844a89ae022d26f52272f
Haribo2011Fortnite <147375633+michas233145@users.noreply.github.com>
2024-11-05 19:15:23 +0100

    Update serwer.mjs
    45r325432

--- Simple Streaming Server/serwer.mjs
+++ Simple Streaming Server/serwer.mjs
@@ -1,28 +1,25 @@
 import fs from 'fs';
-import path from 'path';
-import { URL } from 'url';
 import http from 'http';
-const PORT = 3001;
+import path from 'path';
 
 http.createServer((req, res) => {
-    const fileName = new URL(req.url, `http://${req.headers.host}`).searchParams.get('file');
+    const fileName = new URL(req.url, `http://localhost:3001`).searchParams.get('file');
 
     if (!fileName) {
         res.writeHead(400, { 'Content-Type': 'text/plain' });
         return res.end('Brak nazwy pliku w zapytaniu.');
     }
 
-    const filePath = path.join(process.cwd(), fileName);
+    const filePath = path.resolve(fileName);
+    const stream = fs.createReadStream(filePath);
 
-    fs.access(filePath, fs.constants.F_OK, (err) => {
-        if (err) {
-            res.writeHead(404, { 'Content-Type': 'text/plain' });
-            return res.end('Plik nie został znaleziony.');
-        }
-
-        res.writeHead(200, { 'Content-Type': 'text/plain' });
-        fs.createReadStream(filePath).pipe(res);
+    stream.on('error', () => {
+        res.writeHead(404, { 'Content-Type': 'text/plain' });
+        res.end('Plik nie został znaleziony.');
     });
-}).listen(PORT, () => {
-    console.log(`Serwer działa na http://localhost:${PORT}`);
+
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    stream.pipe(res);
+}).listen(3001, () => {
+    console.log(`Serwer działa na http://localhost:3001`);
 });
